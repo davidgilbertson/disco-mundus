47578326578e26622ab42b01709f32d4d3973e50
David Gilbertson <gilbertson.david@gmail.com>
2020-01-11 10:10:25 +1100

    Async for no reason at all

--- public/data/dataConversionScript.mjs
+++ public/data/dataConversionScript.mjs
@@ -1,17 +1,19 @@
-import fs from 'fs';
+import {promises as fs} from 'fs';
 
-const suburbData = fs.readFileSync('./sydneySuburbs.json', 'utf8');
-const suburbFeatureCollection = JSON.parse(suburbData);
+(async () => {
+  const suburbData = await fs.readFile('./sydneySuburbs.json', 'utf8');
+  const suburbFeatureCollection = JSON.parse(suburbData);
 
-suburbFeatureCollection.features = suburbFeatureCollection.features.map(suburb => ({
-  ...suburb,
-  properties: {
-    ...suburb.properties,
-    center: [suburb.properties.center.lng, suburb.properties.center.lat],
-  },
-}));
+  suburbFeatureCollection.features = suburbFeatureCollection.features.map(suburb => ({
+    ...suburb,
+    properties: {
+      ...suburb.properties,
+      center: [suburb.properties.center.lng, suburb.properties.center.lat],
+    },
+  }));
 
-fs.writeFileSync(
-  './sydneySuburbs.json',
-  JSON.stringify(suburbFeatureCollection, null, 2)
-);
+  await fs.writeFile(
+    './sydneySuburbs.json',
+    JSON.stringify(suburbFeatureCollection, null, 2)
+  );
+})();
